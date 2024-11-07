50f0cbb9043a70203485df6b17b8469823b26708
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-11-07 14:13:21 +0100

    feat: added sign-in authentication to appwrite

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -5,10 +5,15 @@ import { Image } from "react-native";
 import FormField from "@/components/FormField";
 import CustomButton from "@/components/CustomButton";
 import { Link } from "expo-router";
+import { Alert } from "react-native";
+import { getCurrentUser, signIn } from "@/lib/appWrite";
+import { router } from "expo-router";
+import { useGlobalContext } from "@/context/GlobalContext";
 
 import React from "react";
 
 const SignIn = () => {
+  const { setUser, setLoggedIn } = useGlobalContext();
   const [form, setForm] = useState({
     email: "",
     password: "",
@@ -16,8 +21,27 @@ const SignIn = () => {
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleSubmit = () => {};
+  const handleSubmit = async () => {
+    if (!form.email || !form.password) {
+      Alert.alert("Error", "Please fill in the form fields");
+    }
 
+    setIsSubmitting(true);
+
+    try {
+      await signIn(form.email, form.password);
+      const result = await getCurrentUser();
+      setUser(result);
+      setLoggedIn(true);
+
+      Alert.alert("Success", "User signed in successfully");
+      router.replace("/home");
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
   return (
     <SafeAreaView className='bg-primary h-full'>
       <ScrollView>
