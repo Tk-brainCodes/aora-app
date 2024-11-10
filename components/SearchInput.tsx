d50722292c522519f49ff9bec8bd040a987f4419
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-11-10 23:16:48 +0100

    feat: created search input component

---
+++ components/SearchInput.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+
+interface FormFieldProps {
+  title: string;
+  value: string;
+  handleChangeText: (text: string) => void;
+  otherStyles?: string;
+  keyboardType?: "default" | "email-address" | "numeric" | "phone-pad";
+  placeholder: string;
+}
+
+const SearchInput = ({
+  title,
+  value,
+  handleChangeText,
+  otherStyles,
+  keyboardType,
+  ...props
+}: FormFieldProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className='text-base text-gray-100 font-pmedium'>{title}</Text>
+
+      <View className='w-full flex-row h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center border-2 border-black-200'>
+        <TextInput
+          value={value}
+          className='text-white flex-1 font-psemibold text-base'
+          placeholder={props.placeholder}
+          placeholderTextColor='#7b7b8b'
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !showPassword}
+        />
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={
+                !showPassword
+                  ? require("@/assets/icons/eye.png")
+                  : require("@/assets/icons/eye-hide.png")
+              }
+              resizeMode='contain'
+              className='w-6 h-6'
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default SearchInput;
+
+const styles = StyleSheet.create({});
